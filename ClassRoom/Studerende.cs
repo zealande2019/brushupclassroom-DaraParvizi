d2d1d56117854fb8aab17d3d5bd04d3ccadf5d84
Dara Parvizi <dara0046@edu.easj.dk>
2019-02-01 14:16:06 +0100

    Add a new Class "Studernde"

---
+++ ClassRoom/Studerende.cs
@@ -0,0 +1,34 @@
+﻿namespace ClassRoom
+{
+    public class Studerende
+    {
+        private string _navn;
+        private int _fødselsdagmåned;
+        private int _fødselsdag;
+
+        public Studerende( string Navn, int FødselsdagsMåned,int Fødselsdag)
+        {
+            Navn = _navn;
+            _fødselsdagmåned = FødselsdagsMåned;
+            _fødselsdag = Fødselsdag;
+        }
+
+        public string Navn
+        {
+            get { return _navn; }
+            set { _navn = value; }
+        }
+
+        public int FødselsdagsMåned
+        {
+            get { return _fødselsdagmåned; }
+            set { _fødselsdagmåned = value; }
+        }
+
+        public int Fødselsdag
+        {
+            get { return _fødselsdag; }
+            set { _fødselsdag = value; }
+        }
+    }
+}
